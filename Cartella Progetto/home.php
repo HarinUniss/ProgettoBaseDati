270c70266f14973bff5104b61d3f49585f369ac1
alligator99 <58782764+alligator99@users.noreply.github.com>
2022-12-28 13:12:59 +0100

    commit di prova

--- Cartella Progetto/home.php
+++ Cartella Progetto/home.php
@@ -6,12 +6,22 @@
 
 
 
+
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
     <link rel="stylesheet" href="./scss/main.css">
-    <!-- Boxicons CSS -->
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'> <!-- Boxicons CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
 </head>
 
 <body>
@@ -19,6 +29,6 @@
 
     <?php include_once('./Header/header.php'); ?>
     <?php include_once('./Navigatore/nav.php'); ?>
-
+    <?php include_once('./Footer/footer.php'); ?>
 
 </body>
