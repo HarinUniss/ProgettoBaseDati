540012cffd7e43a61c9b470abd8cb7563ec407fc
JohnIasu00 <114760555+JohnIasu00@users.noreply.github.com>
2023-01-18 17:00:53 +0100

    upload sql aggiornato

--- Cartella Progetto/Database/animali.sql
+++ Cartella Progetto/Database/animali.sql
@@ -1,1 +1,2 @@
-UPDATE db_progetto.animali SET nome = 'Susy', razza = 'Corso', eta = 5, sesso = 'f', provenienza = 'Cagliari', pedigree = 0, foto = '../Uploads/Animali/IMG_20230116_213444.jpg', proprietario = 47991, dataora = '2023-01-17 09:09:50' WHERE id_animale = 3830612;
+INSERT INTO db_progetto.animali (id_animale, nome, razza, eta, sesso, provenienza, pedigree, foto, proprietario, dataora) VALUES (3191771, 'Front', 'San bernardo', 7, 'm', 'Torino', 1, '', 3970800, '2023-01-18 04:22:16');
+INSERT INTO db_progetto.animali (id_animale, nome, razza, eta, sesso, provenienza, pedigree, foto, proprietario, dataora) VALUES (3830612, 'Susy', 'Corso', 5, 'f', 'Cagliari', 0, '../Uploads/Animali/IMG_20230116_213444.jpg', 47991, '2023-01-17 09:09:50');
--- Cartella Progetto/Database/razze.sql
+++ Cartella Progetto/Database/razze.sql
@@ -1,1 +1,2 @@
-UPDATE db_progetto.razze SET specie = 'Cane' WHERE razza = 'Corso';
+INSERT INTO db_progetto.razze (razza, specie) VALUES ('Corso', 'Cane');
+INSERT INTO db_progetto.razze (razza, specie) VALUES ('San bernardo', 'Cane');
